42e98038ad5bfc78a818aa19f512d0c850ee3f76
a6hi <saraffabhishek@outlook.com>
2022-05-30 22:11:09 -0400

    Javascript Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, multiple) {
+    if(multiple < 0)
+        return 'ERROR';
+    
+    let answer = '';
 
+    for(let i = 0; i < multiple; i++)   {
+        answer += str;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
