1e6279255fd5957e8337fca6d45ea6c0a1e81cba
a6hi <saraffabhishek@outlook.com>
2022-05-30 22:16:05 -0400

    Javascript Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = '';
+    if(str == '')
+        return '';
 
+    for(let i = str.length-1; i >= 0; i--)   {
+        result += str.charAt(i);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
