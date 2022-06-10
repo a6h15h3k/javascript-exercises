7b961e38de416d6074ea7f174b1672aec2e3766e
a6hi <saraffabhishek@outlook.com>
2022-06-09 21:52:05 -0400

    Javascript Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(initial, remove) {
+    for(let i = 1; i < arguments.length; i++)   {
+        if(initial.includes(arguments[i]) && initial[initial.indexOf(arguments[i])] === arguments[i])
+            initial.splice(initial.indexOf(arguments[i]), 1);
+    }
+    return initial;
 };
 
 // Do not edit below this line
