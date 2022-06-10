3f554be2614f9431780a95d28f538f073dd6cd22
a6hi <saraffabhishek@outlook.com>
2022-06-09 22:12:38 -0400

    Javascript Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    //Return Error if arguments are not Positive Integers or a different Datatype
+    if(typeof(first) != "number" || typeof(second) != "number" || first < 0 || second < 0)
+        return "ERROR";
+    
+    //Decide the starting and ending point
+    let start = first, finish = second, result = 0;
+    if(second < first)  {
+            start = second;
+            finish = first;
+    }
 
+    //Add all of the list
+    for(;start <= finish; start++)   {
+        result += start;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
