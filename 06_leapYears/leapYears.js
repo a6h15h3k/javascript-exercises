f60899e1eeb90982b9a59f4bc805c504f49e203b
a6hi <saraffabhishek@outlook.com>
2022-06-20 22:51:08 -0400

    Javascript Exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
 const leapYears = function() {
-
+    if(arguments[0]%4 == 0 && arguments[0]%100 !=0)  //Leap Year if divisible by 4 but not by 100
+        return true;
+    else if(arguments[0]%400 == 0) //Leap Year if divisible by 400
+        return true;
+    
+    return false; //Else it's not a leap year
 };
 
 // Do not edit below this line
