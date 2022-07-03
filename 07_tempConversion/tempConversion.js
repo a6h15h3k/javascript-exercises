619b56058aae8ab245a803e53156a2ea6d3851c3
a6hi <saraffabhishek@outlook.com>
2022-07-02 23:25:42 -0400

    Javascript Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
 const ftoc = function() {
-
+  let result = (arguments[0] - 32)*5/9;
+  //toFixed to set 1 decimal place then convert the String it returns to a Numebr
+  return Number(result.toFixed(1)); 
 };
 
 const ctof = function() {
-
+  let result = arguments[0]*9/5+32;
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
