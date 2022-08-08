18f7b313402accc4cf90bf704285adab44d5d4db
a6hi <saraffabhishek@outlook.com>
2022-08-08 01:25:34 -0400

    Javascript Exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function(num1, num2) {
-	return num1+num2;
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function(num1, num2) {
-	return num1-num2;
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((sum, position) => {
+    return sum + position;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((result, position) => result * position);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let result = 1;
+  for (let i = 1; i <= number; i++)
+    result *= i;
+  return result;
+
 };
 
 // Do not edit below this line
