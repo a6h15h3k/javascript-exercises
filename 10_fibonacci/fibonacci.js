fbbd32af7f7c6c28091c8661a5f2016d72eb8e41
a6hi <saraffabhishek@outlook.com>
2022-08-08 15:21:35 -0400

    Javascript Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+    if (input < 0)
+        return "OOPS";
 
+    let currentNum = 1, prevNum = 0, temp;
+    for (let i = 1; i < parseInt(input); i++) {
+        temp = currentNum;
+        currentNum += prevNum;
+        prevNum = temp;
+    }
+    return currentNum;
 };
 
 // Do not edit below this line
