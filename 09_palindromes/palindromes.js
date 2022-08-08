d4f1eb7aca5ecb8d32cc115cbe9498e58c489b8d
a6hi <saraffabhishek@outlook.com>
2022-08-08 15:07:55 -0400

    Javascript Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let tempString = '', tempInput = '';
+    for (let i = 0; i < input.length; i++) {
+        if (input.toLowerCase().charCodeAt(i) > 97 && input.toLowerCase().charCodeAt(i) < (97 + 26)) {
+            tempInput += input.toLowerCase().charCodeAt(i);
+            tempString = input.toLowerCase().charCodeAt(i) + tempString;
+        }
+    }
+    if (tempInput == tempString)
+        return true;
+    return false;
 };
 
 // Do not edit below this line
